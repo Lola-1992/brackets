8995542567172e9aeeb96c3ed5fe2b9b9bb8e2a7
Olga <70957483+Lola-1992@users.noreply.github.com>
2020-10-18 16:04:58 +0300

    refactor:
    refactor: change structure of the project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    for (let i = 0; i <= bracketsConfig.length - 1;) {
+        let config = bracketsConfig[i].join('');
+        if (str.includes(config)) {
+            str = str.replace(config, '');
+            i = 0;
+        } else {
+            i = i + 1;
+        }
+    }
+    if (str.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
